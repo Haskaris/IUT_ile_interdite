5848bb1f4ca43d382a773f0ec5d6776629c9f42e
reyneu <reyneu@pc-dg-039-14>
2017-06-02 09:28:01 +0200

    Merge origin/master
    Conflicts:
    	src/view/VueParamJeu.java

---
+++ src/model/aventurier/Messager.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.aventurier;
+
+import java.util.ArrayList;
+import model.Tuile;
+
+/**
+ *
+ * @author reyneu
+ */
+public class Messager extends Aventurier{
+    
+    public Messager(String nom) {
+        super(nom);
+    }
+    
+    @Override
+    public ArrayList<Tuile> getTuilesPossibles(){
+        
+        return super.getTuilesPossibles();
+    }
+    
+    
+}
---
+++ src/model/aventurier/Pilote.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.aventurier;
+
+import java.util.ArrayList;
+import model.Tuile;
+
+/**
+ *
+ * @author reyneu
+ */
+public class Pilote extends Aventurier{
+    
+    private boolean pouvoirUtilise;
+    
+    public Pilote(String nom) {
+        super(nom);
+    }
+
+    /**
+     * @return the pouvoirUtilise
+     */
+    public boolean isPouvoirUtilise() {
+        return pouvoirUtilise;
+    }
+
+    /**
+     * @param pouvoirUtilise the pouvoirUtilise to set
+     */
+    public void setPouvoirUtilise(boolean pouvoirUtilise) {
+        this.pouvoirUtilise = pouvoirUtilise;
+    }
+    
+    @Override
+    public ArrayList<Tuile> getTuilesPossibles(){
+        
+        Tuile[][] tuiles = getGrilleAv().getGrille();
+        ArrayList<Tuile> tuilesPossibles = new ArrayList<>();
+        
+        if (isPouvoirUtilise() == false){
+            for(int i = 0; i < tuiles.length; i++){
+                for(int j = 0; j < tuiles[i].length; i++){
+                    tuilesPossibles.add(tuiles[i][j]);  
+                }
+            }
+        }
+        
+        else {
+            tuilesPossibles = super.getTuilesPossibles();
+        }
+        
+        return tuilesPossibles;
+    }
+}
